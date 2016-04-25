e18e5105c052b7a678ecf8ee79c7fefffea917bd
Bruno Zatta <bzatta@gmail.com>
2016-04-25 17:22:06 -0400

    add logging and error handling

---
+++ server/util/logger.js
@@ -0,0 +1,35 @@
+require('colors');
+var _ = require('lodash'),
+    config = require('../config/config');
+
+var noOperation = function(){};
+
+var consoleLog = config.logging ? console.log.bind(console) : noOperation;
+
+var logger = {
+  log: function() {
+    var args = _.toArray(arguments)
+      .map(function(arg) {
+        if (typeof arg === 'object') {
+          var string = JSON.stringify(arg, null, 2);
+          retrun string.cyn;
+        } else {
+          arg += '';
+          return arg.cyan;
+        }
+      });
+    consoleLog.apply(console, args);
+  },
+  error: function() {
+    var args = _.toArray(arguments)
+      .map(function(arg) {
+        arg = arg.stack || arg;
+        var name = arg.name || '[ERROR]';
+        var log = name.yellow + ' ' + arg.cyan;
+        return log;
+      });
+    consoleLog.apply(console, args);
+  }
+};
+
+module.exports = logger;
