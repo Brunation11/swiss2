4bf5d48d09db684234da7edaa1b186f6c0db9766
Bruno Zatta <bzatta@gmail.com>
2016-04-25 17:21:28 -0400

    update typo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var config = require('/server/config/config'),
     app = require('./server/server'),
-    logger = require('./server/util/logger')
+    logger = require('./server/util/logger');
 
 app.listen(config.port, function() {
   logger.log("Listening on http:localhost:" + config.port);
