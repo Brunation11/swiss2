849553da60ead97aef18049499c097d71bfc7de9
Bruno Zatta <bzatta@gmail.com>
2016-04-30 22:17:56 -0400

    refactor model instance methods

--- server/api/user/model.js
+++ server/api/user/model.js
@@ -1,10 +1,20 @@
 var config = require('../../config/config'),
     crypto = require('crypto'),
     jwt = require('jsonwebtoken'),
+    validator = require('validator'),
     mongoose = require('mongoose'),
     Schema = mongoose.Schema;
+    FolderModel = require('../folder/model');
 
 var UserSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
   username: {
     type: String,
     unique: true,
@@ -13,13 +23,39 @@ var UserSchema = new Schema({
   email: {
     type: String,
     unique: true,
+    required: true,
+  },
+  rootFolder: {
+    type: Schema.Types.ObjectId,
+    ref: 'Folder',
     required: true
+
   },
   hash: String,
   salt: String
 });
 
 UserSchema.methods = {
+  fullName: function() {
+    return this.firstName + " " + this.lastName;
+  },
+  getRootFolder: function() {
+    var user = this,
+        folder = new FolderModel({name: user.username, user: user._id})
+    folder.save();
+    return folder;
+  },
+  setRootFolder: function(next) {
+    this.rootFolder = this.getRootFolder()._id;
+    next();
+  },
+  validateEmail: function(next) {
+    if (validator.isEmail(this.email)) {
+      next();
+    } else {
+      next(new Error("invalid email"));
+    }
+  },
   setPassword: function(password) {
     this.salt = crypto.randomBytes(16).toString('hex');
     this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
@@ -36,5 +72,13 @@ UserSchema.methods = {
     }, config.secrets.jwt);
   }
 };
+
+UserSchema.pre('validate', function(next) {
+  this.setRootFolder(next);
+});
+
+UserSchema.pre('save', function(next) {
+  this.validateEmail(next);
+});
 
 module.exports = mongoose.model('User', UserSchema);
